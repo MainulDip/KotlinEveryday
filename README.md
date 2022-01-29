32d75191a1872efbd7761c74eb8525b442ee5cce
mainuldip <mainuldip@gmail.com>
2022-01-29 18:32:33 -0500

    Looping updating

--- README.md
+++ README.md
@@ -28,6 +28,72 @@ fun main() {
 ```
 
 ### Arithmetic Oparators:
+> Oparators for mathmetical operation like + | - | * | / | %
 ```kt
-val x = 3
+val m = 3 * 4
+println("The value of multiply (m) is $m")
+val d = 3/4 // rerun 0 as Integer, will not return float value
+println("The value of divide (d) is $d")
+val f = 3f / 4f // need to specify float identifier to get the float value
+println("The value of float divide (f) is $f")
+val r = 10 % 3 // modulus or reminder operator
+println("The value of r after modulus operation is $r")
+```
+
+### Check and Boolean
+```kt
+val amIAdult = true
+val amIProgrammer = true
+val anIAdultProgrammer = amIAdult && amIProgrammer
+println("Boolean operation for amIAdultProgrammer = $anIAdultProgrammer")
+//  Also || , == , != , !( val1 != val2 )
+```
+
+### String and Operations
+```kt
+val string = "Use double quotes for string and single quote for Char (Single letter)"
+println(string.uppercase())
+```
+
+### Conditionals
+```kt
+//  Conditionals
+    val conditionVal = 1 + 3
+    if( conditionVal == 2 ) {
+        println("conditionalVal is 2")
+    } else if (conditionVal == 3) {
+        println("value is 3")
+    } else {
+        println ("Neither of the condition is True")
+    }
+
+//  functional style
+    val conditional2 = if ( conditionVal == 2 + 1 ) 2 else "not a match"
+    println("conditional2 value is $conditional2")
+```
+### Default value and null safety
+> ?: , ? , !!
+```kt
+//  kotlin is null safety language
+    val nullValue: Int? = null
+    val number1 = readLine() ?: "0" // Assign default value
+    val number2 = readLine() ?: "0"
+//    val inputResult = number1!!.toInt() + number2!!.toInt()
+    val inputResult = number1.toInt() + number2.toInt()
+//    val userInput = readLine() // return String type
+    println("userInput result is $inputResult")
+```
+
+### List
+```kt
+//  Immutable List
+    val shoppingList = listOf<String>("Hello", "World")
+    println(shoppingList[0])
+    println("shoppingList first value is ${shoppingList[0]}")
+//  Mutable List
+    val mutableShoppingList = mutableListOf<String>("Mutable", "Hello World")
+    mutableShoppingList.add("Good is golden")
+    println(mutableShoppingList[0])
+    println("mutableShoppingList  value is ${mutableShoppingList[0]} ${mutableShoppingList[mutableShoppingList.size - 1]}")
+    println(mutableShoppingList.toString())
 ```
--- intro/src/main/kotlin/Main.kt
+++ intro/src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import java.lang.String.format
+
 //fun main(args: Array<String>) {
 //    println("Hello World!")
 //
@@ -12,11 +14,12 @@ fun main() {
 //    Note: var and val
     var x: Int = 3 // 3L will be 64bit (Long)
 
-    println("This value is $x")
+    println("This value is $x") // print with line break at the end
     x = 4
     println("But The Value is now $x")
     val y: Int = 7 // val is constant
-    print("Hello again $y")
+    print("Hello again $y") // will not put line break at the end
+    println("")
 
 //    types: 3 | 3L | 3.00 (double) | 3.00f (float)
 
@@ -27,4 +30,61 @@ fun main() {
     val checkBoolean: Boolean = false
     val someString: String = "hello world"
     val singleLetter: Char  = 'a' //
+
+//    Mathematical operations
+
+    val m = 3 * 4
+    println("The value of multiply (m) is $m")
+    val d = 3/4 // rerun 0 as Integer, will not return float value
+    println("The value of divide (d) is $d")
+    val f = 3f / 4f // need to specify float identifier to get the float value
+    println("The value of float divide (f) is $f")
+    val r = 10 % 3 // modulus or reminder operator
+    println("The value of r after modulus operation is $r")
+
+//  Boolean Check
+    val amIAdult = true
+    val amIProgrammer = true
+    val anIAdultProgrammer = amIAdult && amIProgrammer
+    println("Boolean operation for amIAdultProgrammer = $anIAdultProgrammer")
+//  Also || , == , != , !( val1 != val2 )
+
+//  String and operations
+    val string = "Use double quotes for string and single quote for Char (Single letter)"
+    println(string.uppercase())
+
+//  Conditionals
+    val conditionVal = 1 + 3
+    if( conditionVal == 2 ) {
+        println("conditionalVal is 2")
+    } else if (conditionVal == 3) {
+        println("value is 3")
+    } else {
+        println ("Neither of the condition is True")
+    }
+
+//  functional style
+    val conditional2 = if ( conditionVal == 2 + 1 ) 2 else "not a match"
+    println("conditional2 value is $conditional2")
+
+//  kotlin is null safety language
+//    val nullValue: Int? = null
+//    val number1 = readLine() ?: "0" // Assign default value
+//    val number2 = readLine() ?: "0"
+////    val inputResult = number1!!.toInt() + number2!!.toInt()
+//    val inputResult = number1.toInt() + number2.toInt()
+////    val userInput = readLine() // return String type
+//    println("userInput result is $inputResult")
+
+//  Lists
+//  Immutable List
+    val shoppingList = listOf<String>("Hello", "World")
+    println(shoppingList[0])
+    println("shoppingList first value is ${shoppingList[0]}")
+//  Mutable List
+    val mutableShoppingList = mutableListOf<String>("Mutable", "Hello World")
+    mutableShoppingList.add("Good is golden")
+    println(mutableShoppingList[0])
+    println("mutableShoppingList  value is ${mutableShoppingList[0]} ${mutableShoppingList[mutableShoppingList.size - 1]}")
+    println(mutableShoppingList.toString())
 }

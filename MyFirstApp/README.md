6a67183966d9da3540096bed3c04a31c5b5e2059
mainuldip <mainuldip@gmail.com>
2022-02-01 02:21:13 -0500

    done firstapp

--- MyFirstApp/README.md
+++ MyFirstApp/README.md
@@ -3,3 +3,8 @@ Here are some personalized docs and instructions set to achieve the building blo
 
 > Layouts, ViewGroup, Widgets and View :
 The layouts are ViewGroup objects, containers that control how their child views are positioned on the screen. Widgets are View objects, UI components such as buttons and text boxes.
+
+### Intent:
+An Intent is an object that provides runtime binding between separate components, such as two activities
+
+> Link: https://developer.android.com/training/basics/firstapp
--- MyFirstApp/app/src/main/java/com/example/myfiirstapp/MainActivity.kt
+++ MyFirstApp/app/src/main/java/com/example/myfiirstapp/MainActivity.kt
@@ -3,9 +3,10 @@ package com.example.myfiirstapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.view.View
 import android.widget.EditText
+
+const val EXTRA_MESSAGE_ON_SCREEN = "com.example.myfiirstapp.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         val editText = findViewById<EditText>(R.id.editTextTextPersonName)
         val message = editText.text.toString()
         val intent = Intent(this, DisplayMessageActivity::class.java).apply {
-            putExtra(EXTRA_MESSAGE, message)
+            putExtra(EXTRA_MESSAGE_ON_SCREEN, message)
         }
         startActivity(intent)
     }

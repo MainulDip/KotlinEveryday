6ffc13896c71f4b2662b69455f4655e53e412776
mainuldip <mainuldip@gmail.com>
2022-04-16 04:22:16 -0400

    lambda again docking

--- JetpackIntro/app/src/main/java/com/websolverpro/jetpackintro/MainActivity.kt
+++ JetpackIntro/app/src/main/java/com/websolverpro/jetpackintro/MainActivity.kt
@@ -4,34 +4,53 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.websolverpro.jetpackintro.ui.theme.JetpackIntroTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            JetpackIntroTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+//        setContent(content = { MyApp() })
+//        setContent(){ MyApp() }
+//        setContent{ MyApp() }
+        /**
+         *  if the last parameter of a function is a function, then a lambda expression passed as the corresponding argument can be placed outside the parentheses
+         */
+        setContent{
+            MyApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp(){
+    JetpackIntroTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            Greeting("Android")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifire: Modifier = Modifier) {
+    androidx.compose.material.Surface(color = Color.LightGray) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(27.dp, 27.dp))
+        Text(text = "Hello $name!", modifier = Modifier.padding(27.dp, 47.dp), color = Color.Blue)
+
+    }
+
 }
 
 //@Preview(name = "font scale 1.5",fontScale = 1.5f)

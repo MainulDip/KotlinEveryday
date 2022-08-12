2e7fa9cdac8b04cced7ef6c209cd22ace04b700f
mainuldip <mainuldip@gmail.com>
2022-08-12 03:36:34 -0400

    lazy with Delegates.observable delegaiton

--- OOPTour/src/main/kotlin/delegation/LazyDelegationSetters.kt
+++ OOPTour/src/main/kotlin/delegation/LazyDelegationSetters.kt
@@ -1,20 +1,21 @@
-import kotlin.properties.ReadOnlyProperty
-import kotlin.properties.ReadWriteProperty
+package delegation
+
 import kotlin.reflect.KProperty
 
 fun main() {
-    println(lazyValue) // first call will print "Hello" and "Again"
-    println(lazyValue) // second call will print only remembered get value which is -> "Again"
+    println(lazyValue1)
+    println(lazyValue1)
 
     println()
-    lazyValue = "something else"
-    println(lazyValue)
+    lazyValue1 = "something else"
+    println(lazyValue1)
 }
 
 // val will make it immutable. for var, custom setValue needs to be created.
 var customSetterValue: Any? = null
-var lazyValue: Any by lazy {
+private var lazyValue1: Any by lazy {
 //    println("World")
+//    "asdf"
 }
 
 private operator fun <T : Any> Lazy<T>.getValue(t: T?, property: KProperty<*>): Any {
@@ -25,7 +26,6 @@ private operator fun <T : Any> Lazy<T>.getValue(t: T?, property: KProperty<*>): 
 //    else
 //        println("Testing")
 //        return t as T
-////    return t
 }
 
 private operator fun <T : Any> Lazy<T>.setValue(t: T?, property: KProperty<*>, t1: T) {

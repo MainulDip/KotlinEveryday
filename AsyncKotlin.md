d5f51ecb53a8ad674c56c203cc17109bb93ec5b8
mainuldip <mainuldip@gmail.com>
2022-09-06 03:37:10 -0400

    mutable state concurrency task

--- AsyncKotlin.md
+++ AsyncKotlin.md
@@ -5,3 +5,6 @@ https://kotlinlang.org/docs/async-programming.html#futures-promises-and-others
 https://hackernoon.com/asynchronous-programming-techniques-with-kotlin-fg9l3wjn
 
 https://www.baeldung.com/java-asynchronous-programming
+
+### Shared mutable state and concurrency:
+https://kotlinlang.org/docs/shared-mutable-state-and-concurrency.html
--- IntroCoroutines/src/tasks/Request3Callbacks.kt
+++ IntroCoroutines/src/tasks/Request3Callbacks.kt
@@ -38,13 +38,13 @@ fun loadContributorsCallbacks(service: GitHubService, req: RequestData, updateRe
 }
 
 inline fun <T> Call<T>.onResponse(crossinline callback: (Response<T>) -> Unit) {
-    enqueue(object : Callback<T> {
-        override fun onResponse(call: Call<T>, response: Response<T>) {
-            callback(response)
-        }
-
-        override fun onFailure(call: Call<T>, t: Throwable) {
-            log.error("Call failed", t)
-        }
-    })
+//    enqueue(object : Callback<T> {
+//        override fun onResponse(call: Call<T>, response: Response<T>) {
+//            callback(response)
+//        }
+//
+//        override fun onFailure(call: Call<T>, t: Throwable) {
+//            log.error("Call failed", t)
+//        }
+//    })
 }

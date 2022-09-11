c0b829895e041546c25b16edd5af381f38fda567
mainuldip <mainuldip@gmail.com>
2022-09-11 05:57:59 -0400

    kotlin destructuring code fixing

--- OOPTour/src/main/kotlin/destructuring/DestructuringRegularClass.kt
+++ OOPTour/src/main/kotlin/destructuring/DestructuringRegularClass.kt
@@ -5,37 +5,13 @@ package destructuring
 class Order(val lines: List<OrderLine>)
 
 class OrderLine(val name: String, val price: Int) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as OrderLine
-
-        if (name != other.name) return false
-        if (price != other.price) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = name.hashCode()
-        result = 31 * result + price
-        return result
-    }
-
-    override fun toString(): String {
-        return "OrderLine(name='$name', price=$price)"
-    }
-
-    operator fun component1(): Any {
+    operator fun component1(): String {
         return name
     }
 
-    operator fun component2(): Any {
+    operator fun component2(): Int {
         return price
     }
-
-
 }
 
 fun mapTester(){
@@ -43,12 +19,15 @@ fun mapTester(){
         lines = listOf(OrderLine("Tomato", 2), OrderLine("Garlic", 3), OrderLine("Chives", 2))
     )
 
-    val namesprice = order.lines.map { (name, price) -> "$name : $price" }
-    println(namesprice)
-//    val namesDestructing = order.lines.map { (name, price) -> name  }
-    val totalPrice = order.lines.map { it.price }.sum()
-//    println("names: $names && totalPrice: $totalPrice") // names: [Tomato, Garlic, Chives] && totalPrice: 7
+    val nameAndPrice = order.lines.map { (name, price) -> "$name : $price" }
+    println(nameAndPrice) // [Tomato : 2, Garlic : 3, Chives : 2]
 }
+
+fun main() {
+    mapTester() // // [Tomato : 2, Garlic : 3, Chives : 2]
+}
+
+
 
 fun flatMapTesting(){
     val orders: List<Order> = listOf(
@@ -78,9 +57,4 @@ fun flatMapTesting(){
     val deepList = listOf(listOf(1), listOf(2, 3, "77"), listOf(4, 7, 6))
 //    val flattenNotWorking = listOf(listOf(1), listOf(2, 3, "77"), listOf(4, 5, 6), OrderLine("Tomato", 2)) // will not work because of OrderLine insertion
     println(deepList.flatten()) // [1, 2, 3, 77, 4, 7, 6]
-}
-
-fun main() {
-    mapTester()
-    flatMapTesting()
 }

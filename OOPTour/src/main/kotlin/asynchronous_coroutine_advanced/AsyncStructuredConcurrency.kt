fe5b63b6642843df0e6760220cf2dc6a69b12155
mainuldip <mainuldip@gmail.com>
2022-10-14 03:31:30 -0400

    Kotlin Coroutine Structured Async

--- OOPTour/src/main/kotlin/asynchronous_coroutine_advanced/AsyncStructuredConcurrency.kt
+++ OOPTour/src/main/kotlin/asynchronous_coroutine_advanced/AsyncStructuredConcurrency.kt
@@ -1,5 +1,27 @@
 package asynchronous_coroutine_advanced
 
-fun main() {
-    println("Bismillah")
+import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
+
+fun main() = runBlocking<Unit> {
+    val time = measureTimeMillis {
+        println("The answer is ${concurrentSum()}")
+    }
+    println("Completed in $time ms")
+}
+// structured parent coroutine
+suspend fun concurrentSum(): Int = coroutineScope {
+    val one = async { childSuspendOne() }
+    val two = async { childSuspendTwo() }
+    one.await() + two.await()
+}
+
+suspend fun childSuspendOne(): Int {
+    delay(1000L) // pretend we are doing something useful here
+    return 13
+}
+
+suspend fun childSuspendTwo(): Int {
+    delay(1000L) // pretend we are doing something useful here, too
+    return 29
 }
---
+++ OOPTour/src/main/kotlin/asynchronous_coroutine_advanced/StructuredConcurrencyAsyncErrorHandling.kt
@@ -0,0 +1,31 @@
+package asynchronous_coroutine_advanced
+
+import kotlinx.coroutines.*
+/*
+* Structured way, if something goes wrong inside the code of the parent function,
+* and it throws an exception, all the coroutines that were launched in its scope will be cancelled
+*/
+
+fun main() = runBlocking<Unit> {
+    try {
+        failedConcurrentSum()
+    } catch(e: ArithmeticException) {
+        println("Computation failed with ArithmeticException")
+    }
+}
+
+suspend fun failedConcurrentSum(): Int = coroutineScope {
+    val one = async<Int> {
+        try {
+            delay(Long.MAX_VALUE) // Emulates very long computation
+            42
+        } finally {
+            println("First child was cancelled")
+        }
+    }
+    val two = async<Int> {
+        println("Second child throws an exception")
+        throw ArithmeticException()
+    }
+    one.await() + two.await()
+}
